479c7dfa89793c6a92e285ae3fc5e3b635f4d0fe
Christian Predikant <christian.predikant@gmail.com>
2013-09-15 23:47:35 +0200

    Database Configuration and an baseset of entities

--- ejb/src/main/java/com/prodyna/pac/conference/TravisTest.java
+++
@@ -1,5 +0,0 @@
-package com.prodyna.pac.conference;
-
-public class TravisTest {
-
-}
---
+++ ejb/src/main/java/com/prodyna/pac/conference/client/model/Conference.java
@@ -0,0 +1,70 @@
+package com.prodyna.pac.conference.client.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="CONFERENCE")
+public class Conference implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private long id;
+	
+	private String name;
+	
+	private String description;
+	
+	private Date start;
+	
+	private Date end;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Date getStart() {
+		return start;
+	}
+
+	public void setStart(Date start) {
+		this.start = start;
+	}
+
+	public Date getEnd() {
+		return end;
+	}
+
+	public void setEnd(Date end) {
+		this.end = end;
+	}
+	
+}
