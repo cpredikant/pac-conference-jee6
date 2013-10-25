d256c80a09e3849d5906396b7d03164416d33caf
Christian Predikant <christian.predikant@gmail.com>
2013-10-26 00:25:30 +0200

    RoomService test started

---
+++ conference-ejb/src/test/java/com/prodyna/pac/conference/service/test/RoomServiceTest.java
@@ -0,0 +1,209 @@
+package com.prodyna.pac.conference.service.test;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.junit.InSequence;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+
+import com.prodyna.pac.conference.client.api.ConferenceService;
+import com.prodyna.pac.conference.client.api.RoomService;
+import com.prodyna.pac.conference.client.model.Conference;
+import com.prodyna.pac.conference.client.model.Room;
+
+@RunWith(Arquillian.class)
+public class RoomServiceTest {
+
+	@Inject
+	Logger logger;
+
+	@Inject
+	private RoomService roomService;
+	
+	@Inject
+	private ConferenceService conferenceService;
+
+	@Deployment
+	public static Archive<?> createTestArchive() {
+
+		WebArchive war = ShrinkWrap.create(WebArchive.class,
+				"RoomServiceTest.war");
+		war.addPackages(true, "com.prodyna.pac.conference");
+		war.addAsResource("META-INF/test-beans.xml", "META-INF/beans.xml");
+		war.addAsResource("META-INF/test-persistence.xml",
+				"META-INF/persistence.xml");
+		return war;
+	}
+
+	@Test
+	@InSequence(1)
+	public void createRoomTest() throws Exception {
+		Conference conference = new Conference();
+		conference.setDescription("A description");
+		conference.setName("A Name");
+		conference.setEnd(new Date());
+
+		conferenceService.createConference(conference);
+
+		Assert.assertTrue(conference.getId() > 0);
+		
+		Room room = new Room();
+		room.setCapacity(1000);
+		room.setName("Audimax");
+		room.setConference(conference);
+		
+		roomService.createRoom(room);
+		
+		Assert.assertTrue(room.getId() > 0);
+		
+	}
+	
+	@Test
+	@InSequence(2)
+	public void findRoomsByConferenceIdTest() throws Exception {
+		Conference conference = new Conference();
+		conference.setDescription("A description");
+		conference.setName("A Name");
+		conference.setEnd(new Date());
+
+		conferenceService.createConference(conference);
+
+		Assert.assertTrue(conference.getId() > 0);
+		
+		Room room = new Room();
+		room.setCapacity(1000);
+		room.setName("Audimax");
+		room.setConference(conference);
+		
+		roomService.createRoom(room);
+		
+		Assert.assertTrue(room.getId() > 0);
+		
+		List<Room> rooms = roomService.findRoomsByConferenceId(conference.getId());
+		
+		Assert.assertTrue(rooms.size() > 0);
+		Assert.assertEquals(room.getName(), rooms.get(0).getName());
+	}
+
+//	@Test
+//	@InSequence(2)
+//	public void updateConferenceTest() throws Exception {
+//		Conference conference = new Conference();
+//		conference.setDescription("A description");
+//		conference.setName("A Name");
+//		conference.setEnd(new Date());
+//
+//		conferenceService.createConference(conference);
+//
+//		Assert.assertTrue(conference.getId() > 0);
+//
+//		conference.setName("Another name");
+//
+//		Conference updatedConference = conferenceService
+//				.updateConference(conference);
+//
+//		Assert.assertTrue(updatedConference.getId() > 0);
+//
+//		Assert.assertEquals("Another name", updatedConference.getName());
+//
+//	}
+//
+//	@Test
+//	@InSequence(3)
+//	public void deleteConferenceTest() throws Exception {
+//		Conference conference = new Conference();
+//		conference.setDescription("A description");
+//		conference.setName("A Name");
+//		conference.setEnd(new Date());
+//
+//		conferenceService.createConference(conference);
+//
+//		Assert.assertTrue(conference.getId() > 0);
+//
+//		conferenceService.deleteConference(conference);
+//
+//		Conference deletedConference = conferenceService
+//				.findConferenceById(conference.getId());
+//
+//		Assert.assertNull(deletedConference);
+//
+//	}
+//
+//	@Test
+//	@InSequence(4)
+//	public void findConferenceByIdTest() throws Exception {
+//		Conference conference = new Conference();
+//		conference.setDescription("A description");
+//		conference.setName("findyById");
+//		conference.setEnd(new Date());
+//
+//		conferenceService.createConference(conference);
+//
+//		Assert.assertTrue(conference.getId() > 0);
+//
+//		Conference foundConference = conferenceService
+//				.findConferenceById(conference.getId());
+//
+//		Assert.assertEquals(conference.getName(), foundConference.getName());
+//
+//	}
+//
+//	@Test
+//	@InSequence(5)
+//	public void findConferenceByNameTest() throws Exception {
+//		Conference conference = new Conference();
+//		conference.setDescription("A description");
+//		conference.setName("findyByname");
+//		conference.setEnd(new Date());
+//
+//		conferenceService.createConference(conference);
+//
+//		Assert.assertTrue(conference.getId() > 0);
+//
+//		List<Conference> foundConference = conferenceService
+//				.findConferenceByName("findyByname");
+//
+//		Assert.assertTrue(foundConference.size() > 0);
+//
+//		Assert.assertEquals("findyByname", foundConference.get(0).getName());
+//	}
+//	
+//	
+//	@Test
+//	@InSequence(6)
+//	public void findAllTest() throws Exception {
+//		
+//		Conference conference = new Conference();
+//		conference.setDescription("A description");
+//		conference.setName("findAll1");
+//		conference.setEnd(new Date());
+//
+//		conferenceService.createConference(conference);
+//		
+//		
+//		Conference conference1 = new Conference();
+//		conference1.setDescription("A description");
+//		conference1.setName("findAll2");
+//		conference1.setEnd(new Date());
+//
+//		conferenceService.createConference(conference1);
+//
+//		Assert.assertTrue(conference.getId() > 0);
+//
+//		List<Conference> foundConference = conferenceService.findAll();
+//
+//		Assert.assertTrue(foundConference.size() > 0);
+//
+//	}
+
+}
