350c97d90fadf6c2c38f467e4f72dbe19e74b4bc
Christian Predikant <christian.predikant@gmail.com>
2013-11-13 20:45:48 +0100

    REST done

--- conference-rest-web/src/main/java/com/prodyna/pac/conference/web/rest/impl/secure/SpeakerHasTalkSecureRestServiceImpl.java
+++ conference-rest-web/src/main/java/com/prodyna/pac/conference/web/rest/impl/secure/SpeakerHasTalkSecureRestServiceImpl.java
@@ -11,37 +11,83 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
 
+import org.jboss.resteasy.spi.InternalServerErrorException;
+import org.jboss.resteasy.spi.NotFoundException;
 import org.slf4j.Logger;
 
 import com.prodyna.pac.conference.api.SpeakerHasTalkService;
+import com.prodyna.pac.conference.api.SpeakerService;
+import com.prodyna.pac.conference.api.TalkService;
+import com.prodyna.pac.conference.exception.SpeakerNotFoundException;
+import com.prodyna.pac.conference.exception.TalkNotFoundException;
+import com.prodyna.pac.conference.model.Speaker;
+import com.prodyna.pac.conference.model.Talk;
 import com.prodyna.pac.conference.web.rest.api.secure.SpeakerHasTalkSecureRestService;
 
 @Path("/private")
 @RequestScoped
-public class SpeakerHasTalkSecureRestServiceImpl implements SpeakerHasTalkSecureRestService {
-	
-	@Inject
-	private Logger logger;
-	
+public class SpeakerHasTalkSecureRestServiceImpl implements
+		SpeakerHasTalkSecureRestService {
+
 	@Inject
 	private SpeakerHasTalkService speakerHasTalkService;
+
+	@Inject
+	private TalkService talkService;
+
+	@Inject
+	private SpeakerService speakerService;
+
+	@Inject
+	private Logger logger;
 
 	@POST
 	@Path("/speaker/{speakerId:[0-9][0-9]*}/assign/{talkId:[0-9][0-9]*}")
 	@Override
-	public Response assign(@PathParam("speakerId") String speakerId, @PathParam("talkId") String talkId) {
-		// TODO Auto-generated method stub
-		return null;
+	public Response assign(@PathParam("speakerId") String speakerId,
+			@PathParam("talkId") String talkId) {
+		try {
+			Talk talk = talkService.findTalkById(Long.valueOf(talkId));
+			Speaker speaker = speakerService.findSpeakerById(Long
+					.valueOf(speakerId));
+			speakerHasTalkService.assign(speaker, talk);
+		} catch (TalkNotFoundException e) {
+			logger.error("Talk not Found by id " + talkId, e);
+			throw new NotFoundException(e);
+		} catch (SpeakerNotFoundException e) {
+			logger.error("Speaker not Found by id " + speakerId, e);
+			throw new NotFoundException(e);
+		} catch (Exception e) {
+			logger.error("Error assign Speaker and Talk", e);
+			throw new InternalServerErrorException(e);
+		}
+
+		return Response.ok().build();
 	}
-	
+
 	@DELETE
 	@Path("/speaker/{speakerId:[0-9][0-9]*}/unassign/{talkId:[0-9][0-9]*}")
 	@Override
-	public Response unassign(@PathParam("speakerId") String speakerId, @PathParam("talkId") String talkId) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	public Response unassign(@PathParam("speakerId") String speakerId,
+			@PathParam("talkId") String talkId) {
+		try {
+			Talk talk = talkService.findTalkById(Long.valueOf(talkId));
+			Speaker speaker = speakerService.findSpeakerById(Long
+					.valueOf(speakerId));
+			speakerHasTalkService.unassign(speaker, talk);
+		} catch (TalkNotFoundException e) {
+			logger.error("Talk not Found by id " + talkId, e);
+			throw new NotFoundException(e);
+		} catch (SpeakerNotFoundException e) {
+			logger.error("Speaker not Found by id " + speakerId, e);
+			throw new NotFoundException(e);
+		} catch (Exception e) {
+			logger.error("Error unassign Speaker and Talk", e);
+			throw new InternalServerErrorException(e);
 
-	
+		}
+
+		return Response.ok().build();
+	}
 
 }
--- conference-rest-web/src/main/java/com/prodyna/pac/conference/web/rest/impl/unsecure/SpeakerHasTalkUnsecureRestServiceImpl.java
+++ conference-rest-web/src/main/java/com/prodyna/pac/conference/web/rest/impl/unsecure/SpeakerHasTalkUnsecureRestServiceImpl.java
@@ -3,6 +3,8 @@
  */
 package com.prodyna.pac.conference.web.rest.impl.unsecure;
 
+import java.util.List;
+
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
@@ -10,9 +12,19 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
 
+import org.jboss.resteasy.spi.InternalServerErrorException;
+import org.jboss.resteasy.spi.NotFoundException;
 import org.slf4j.Logger;
 
 import com.prodyna.pac.conference.api.SpeakerHasTalkService;
+import com.prodyna.pac.conference.api.SpeakerService;
+import com.prodyna.pac.conference.api.TalkService;
+import com.prodyna.pac.conference.exception.SpeakerHasTalkNotFoundException;
+import com.prodyna.pac.conference.exception.SpeakerNotFoundException;
+import com.prodyna.pac.conference.exception.TalkNotFoundException;
+import com.prodyna.pac.conference.model.Speaker;
+import com.prodyna.pac.conference.model.SpeakerHasTalk;
+import com.prodyna.pac.conference.model.Talk;
 import com.prodyna.pac.conference.web.rest.api.unsecure.SpeakerHasTalkUnsecureRestService;
 
 @Path("/public")
@@ -24,31 +36,83 @@ public class SpeakerHasTalkUnsecureRestServiceImpl implements
 	private SpeakerHasTalkService speakerHasTalkService;
 
 	@Inject
+	private TalkService talkService;
+
+	@Inject
+	private SpeakerService speakerService;
+
+	@Inject
 	private Logger logger;
 
 	@GET
 	@Path("/talksBySpeaker/{speakerId:[0-9][0-9]*}/{talkId:[0-9][0-9]*}")
 	@Override
-	public Response findSpeakerHasTalkBySpeakerAndTalk(@PathParam("speakerId") String speakerId,
+	public Response findSpeakerHasTalkBySpeakerAndTalk(
+			@PathParam("speakerId") String speakerId,
 			@PathParam("talkId") String talkId) {
-		// TODO Auto-generated method stub
-		return null;
+		SpeakerHasTalk speakerHasTalk = null;
+		try {
+			Talk talk = talkService.findTalkById(Long.valueOf(talkId));
+			Speaker speaker = speakerService.findSpeakerById(Long
+					.valueOf(speakerId));
+			speakerHasTalk = speakerHasTalkService
+					.findSpeakerHasTalkBySpeakerAndTalk(speaker, talk);
+		} catch (SpeakerHasTalkNotFoundException e) {
+			logger.error("SpeakerHasTalk not found", e);
+			throw new NotFoundException(e);
+		} catch (TalkNotFoundException e) {
+			logger.error("Talk not Found by id " + talkId, e);
+			throw new NotFoundException(e);
+		} catch (SpeakerNotFoundException e) {
+			logger.error("Speaker not Found by id " + speakerId, e);
+			throw new NotFoundException(e);
+		} catch (Exception e) {
+			logger.error("Error list Speakers by Talk", e);
+			throw new InternalServerErrorException(e);
+
+		}
+		
+		return Response.ok(speakerHasTalk).build();
+	
 	}
 
 	@GET
 	@Path("/speakersByTalk/{talkId:[0-9][0-9]*}")
 	@Override
 	public Response listSpeakersByTalk(@PathParam("talkId") String talkId) {
-		// TODO Auto-generated method stub
-		return null;
+		List<Speaker> speakers = null;
+		try {
+			Talk talk = talkService.findTalkById(Long.valueOf(talkId));
+			speakers = speakerHasTalkService.findSpeakersByTalk(talk);
+
+		} catch (TalkNotFoundException e) {
+			logger.error("Talk not Found by id " + talkId, e);
+			throw new NotFoundException(e);
+		} catch (Exception e) {
+			logger.error("Error list Speakers by Talk", e);
+			throw new InternalServerErrorException(e);
+		}
+		return Response.ok(speakers).build();
 	}
 
 	@GET
 	@Path("/talsBySpeaker/{speakerId:[0-9][0-9]*}")
 	@Override
 	public Response listTalksBySpeaker(@PathParam("speakerId") String speakerId) {
-		// TODO Auto-generated method stub
-		return null;
+		List<Talk> talks = null;
+		try {
+			Speaker speaker = speakerService.findSpeakerById(Long
+					.valueOf(speakerId));
+			talks = speakerHasTalkService.findTalksBySpeaker(speaker);
+
+		} catch (SpeakerNotFoundException e) {
+			logger.error("Speaker not Found by id " + speakerId, e);
+			throw new NotFoundException(e);
+		} catch (Exception e) {
+			logger.error("Error list Talks by Speaker", e);
+			throw new InternalServerErrorException(e);
+		}
+		return Response.ok(talks).build();
 	}
 
 }
